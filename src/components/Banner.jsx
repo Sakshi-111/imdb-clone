6e03bf96a5b745d9e15e613e1ee3966d676cc858
Sakshi-111 <sakshayaggarwal@gmail.com>
2024-10-16 00:24:59 +0530

    BANNER

--- src/components/Banner.jsx
+++ src/components/Banner.jsx
@@ -2,8 +2,8 @@ import React from 'react'
 
 function Banner() {
   return (
-    <div className='h-[20vh] md:h-[75vh] bg-cover bg-center flex items-end' style={{backgroundImage : `url(https://i.pinimg.com/originals/29/7d/e0/297de0761b0c756266d74ca50d03cc1d.jpg)`}}>
-         <div className='text-white text-2xl text-center w-full bg-gray-900/60 p-4 '>Avengers Endgame</div>
+    <div className='h-[20vh] md:h-[75vh] bg-cover bg-center flex items-end' style={{backgroundImage : `url(https://image.tmdb.org/t/p/original/wWba3TaojhK7NdycRhoQpsG0FaH.jpg`}}>
+         <div className='text-white text-2xl text-center w-full bg-gray-900/60 p-4 '>EXPLORE</div>
     </div>
   )
 }
